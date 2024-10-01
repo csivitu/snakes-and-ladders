b7f0736e68bef6b5e356f8dbf460f4b60599ea4a
MKDev <58660200+MKDev121@users.noreply.github.com>
2024-10-01 21:22:49 +0530

    Added GUI window

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from time import sleep
 from random import randint
 
 
-choice=int(input("Do you want to play 2 player or player vs computer : (0 for computer and 1 for 2 player)\n>>"))
-while choice not in [0,1]:
-    print("\nINVALID INPUT!!!\n")    
-    choice=int(input("Do you want to play 2 player or player vs computer : (0 for computer and 1 for 2 player)\n>>"))
+choice=0
+
 """ if choice:
     name_1=input("Enter Player 1 Name \n>>")
     name_2=input("Enter Player 2 Name \n>>")
@@ -378,7 +376,7 @@ pg_img_loader=img_loader()
 is_both_players_on_same_square=1
 p1.pos_y-=2
 p2.pos_y+=2
-running_load=running_name=True
+running_load=running_name=running_choice=True
 font_70 = Font(None, 70)
 font_36 = Font("resources/test.ttf", 36)
 r=g=b=255
@@ -392,6 +390,30 @@ temp=temp1=1
 if not is_started:
     user_name_1=""
     user_name_2=""
+    while running_choice:
+
+        prompt= font_70.render("Select the mode", True, (255,255,255))
+        single_font=font_70.render("Single",True,(255,255,255))
+        double_font=font_70.render("Double",True,(255,255,255))
+        window.fill((0,0,0))
+        window.blit(prompt,(285,120))
+        single_box=pg.draw.rect(window,(0, 255, 0),(window.get_width()/2-300,window.get_height()/2,250,100),border_radius=100)
+        window.blit(single_font,(window.get_width()/2-250,window.get_height()/2+30))
+        two_player_box=pg.draw.rect(window,(0, 0, 255),(window.get_width()/2,window.get_height()/2,250,100),border_radius=100)
+        window.blit(double_font,(window.get_width()/2+50,window.get_height()/2+30))
+        for event in pg.event.get():  
+            if event.type==pg.QUIT:
+                running=running_choice=running_name=False
+        if event.type==pg.MOUSEBUTTONDOWN:
+            if event.button==1:
+                mouse_pos=pg.mouse.get_pos()
+                if single_box.collidepoint(mouse_pos):
+                    choice=0
+                    running_choice=False
+                elif two_player_box.collidepoint(mouse_pos):
+                    choice=1
+                    running_choice=False
+        pg.display.flip()
     while running_name:
         if not running_name:
             break
